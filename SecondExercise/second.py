82a37f3991bc1b3fdeae19e5fe9cece92ed1de55
Lifercontreras <lfcontrerasc@ufpso.edu.co>
2025-09-23 20:01:10 -0600

    parcial

---
+++ SecondExercise/second.py
@@ -0,0 +1,27 @@
+from nis import match
+
+
+def funtion ():
+    for n in range(2,1_000_000):
+        if 3*n**2+ 50*n :
+            return n
+print ("El umbral es:" + funtion)
+
+
+def funtion ():
+    for n in range(2,1_000_000):
+        if 8*n *match.log2*n + 200:
+            return n
+print ("El umbral es:" + funtion)
+
+def funtion ():
+    for n in range(2,1_000_000):
+        if 0.2*n**3 :
+            return n
+print ("El umbral es:" + funtion)
+
+def funtion ():
+    for n in range(2,1_000_000):
+        if 2**n:
+            return n
+print ("El umbral es:" + funtion)
